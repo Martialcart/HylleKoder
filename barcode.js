ce702120e95bdbca4ce7a9a16b6cc8a98d3f75c3
Jan_Olav_Berg <janolav91@hotmail.com>
2025-09-26 11:40:35 +0200

    first draft

---
+++ barcode.js
@@ -0,0 +1,49 @@
+const barcodeInput = document.querySelector("#barcodeInput");
+const barcodeItemDefault = document.querySelector("#barcodeItem").cloneNode(true);
+const barcodeContainer = document.querySelector("#barcodeContainer");
+const formatCode = "SEPARL";
+
+let barcodeIdNumber = 0;
+
+barcodeInput.addEventListener("keyup", (e) => barcodeInputHandler(e));
+
+function barcodeInputHandler(e) {
+    switch(e.key) {
+	case 'Enter': createBarcodeItem(); break;
+	default: writeBarcode(e); 
+    }
+}
+
+function createBarcodeItem() {
+    barcodeIdNumber++;
+
+    const tempBarcodeItem = barcodeItemDefault.cloneNode("true");//store clone of default
+    const barInput = tempBarcodeItem.querySelector("input");
+    
+    //set unique id's
+    tempBarcodeItem.id += barcodeIdNumber;
+    barInput.id += barcodeIdNumber;
+    tempBarcodeItem.querySelector("canvas").id += barcodeIdNumber;
+
+    //attach eventlistener to input
+    barInput.addEventListener("keyup",(e) => barcodeInputHandler(e)); 
+
+    //add barcodeItem to document 
+    barcodeContainer.appendChild(tempBarcodeItem);
+    
+    //set focus to new barcodeInput
+    barInput.focus();
+}
+
+function writeBarcode(e) {
+    const input = e.srcElement;
+    if(input.value !== "") {
+	const id = e.srcElement.parentNode.querySelector("canvas").id;
+	console.log(id);
+	JsBarcode(("#" + id), (formatCode + input.value), {
+	    displayValue: false
+	}); 
+    }
+}
+
+
